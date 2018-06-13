6b1f8fc14f420397bc06120dacc139e298f545fb
Sammy Haq <sammy.haq1@gmail.com>
2018-06-13 17:00:43 -0400

    Uncommented all comment code.

--- pi/gaitmateTest.py
+++ pi/gaitmateTest.py
@@ -13,12 +13,12 @@ import RPi.GPIO as GPIO
 def main():
     controller = Gaitmate.Gaitmate(0x68, 27, 17, 22, 23, 23);
 
-#    controller.testBuzzer();
-#    controller.testHaptic();
+    controller.testBuzzer();
+    controller.testHaptic();
     controller.testGyro();
-#    controller.testButton();
-#    controller.testLaser();
-#    controller.testLED();
+    controller.testButton();
+    controller.testLaser();
+    controller.testLED();
     GPIO.cleanup();
 
 main();
