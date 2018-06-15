d9bc6e130dd4807ea7e630be9f70c0dd36fbe272
Sammy Haq <sammy.haq1@gmail.com>
2018-06-15 15:41:45 -0400

    All data collecting code works.

--- pi/Gaitmate.py
+++ pi/Gaitmate.py
@@ -92,7 +92,7 @@ class Gaitmate:
             return;
         
         # Initializing write file to have the name of the local time and date.
-        fileName = time.strftime("logs/%m-%d-%y_%H%M%S", time.localtime());
+        fileName = time.strftime("/home/pi/gaitmate/pi/logs/%m-%d-%y_%H%M%S", time.localtime());
         print("Creating " + fileName + "..");
 
         self.writer = SaveFileHelper.SaveFileHelper(fileName);
