88e430f46b304006d453f937aa41a91c76ca1446
Sammy Haq <sammy.haq1@gmail.com>
2018-06-20 12:47:49 -0400

    Updated resetGPIO script.

--- pi/resetGPIO.py
+++ pi/resetGPIO.py
@@ -1,8 +1,37 @@
 import RPi.GPIO as GPIO
-
+import State
+import MPU6050
+import OutputComponent
+import Button
 
 def main():
-    GPIO.cleanup()
 
+    GPIO.setwarnings(False);
+    print("Resetting all pins..")
+
+    gyroAddress = 0x68
+    buzzerPin = 17
+    hapticPin = 27
+    buttonPin = 6
+    laserPin = 5
+    ledPin = 25
+    
+    gyroAddress = gyroAddress
+    buzzerPin = buzzerPin
+    hapticPin = hapticPin
+    buttonPin = buttonPin
+    laserPin = laserPin
+    ledPin = ledPin
+
+    state = State.State()
+    gyro = MPU6050.MPU6050(gyroAddress)
+    buzzer = OutputComponent.OutputComponent(buzzerPin)
+    haptic = OutputComponent.OutputComponent(hapticPin)
+    button = Button.Button(buttonPin)
+    laser = OutputComponent.OutputComponent(laserPin)
+    led = OutputComponent.OutputComponent(ledPin)
+
+    GPIO.cleanup()
+    print("  ..done.");
 
 main()
