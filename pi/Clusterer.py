93b1e9da47fc9d4cf68e3de7f475f1661426fa82
Sammy Haq <sammy.haq1@gmail.com>
2018-06-21 12:15:36 -0400

    Made Clusterer.py PEP8 compliant.

--- pi/Clusterer.py
+++ pi/Clusterer.py
@@ -3,23 +3,29 @@ import numpy as np
 import LoadFileHelper
 import os
 
+
 class Clusterer:
 
     def __init__(self, filenames):
-        self.rawVariance = [];
+        self.rawVariance = []
 
         for i in range(len(filenames)):
-            l = LoadFileHelper.LoadFileHelper(filenames[i])
-            
-            # Checking to see if file was incorrectly saved, before parsing. Prevents blank files from being parsed and ruining clustering.
-            if (l.isBlank()):
+            loader = LoadFileHelper.LoadFileHelper(filenames[i])
+
+            # Checking to see if file was incorrectly saved, before parsing.
+            # Prevents blank files from being parsed and ruining clustering.
+            if (loader.isBlank()):
                 continue
             else:
-                l.parseData()  # parsing data
+                loader.parseData()  # parsing data
 
-            self.rawVariance.append([l.getDataVariance_X(), l.getDataVariance_Y(), l.getDataVariance_Z()])
-    
-        self.results = KMeans(n_clusters=3, random_state=0).fit(np.array(self.rawVariance))
+            self.rawVariance.append([loader.getDataVariance_X(),
+                                     loader.getDataVariance_Y(),
+                                     loader.getDataVariance_Z()])
+
+        self.results = KMeans(n_clusters=3,
+                              random_state=0).fit(np.array(self.rawVariance))
+
 
 def ClusterInterface():
     print("\n####################")
@@ -30,12 +36,12 @@ def ClusterInterface():
     walkingFileList = os.listdir("logs/walkingData")
     print("\t\t"+str(len(walkingFileList))+" data points found.")
     print("\t\t..done.")
-    
+
     print("\tpulling from 'logs/standingData..'")
     standingFileList = os.listdir("logs/standingData")
     print("\t\t"+str(len(standingFileList))+" data points found.")
     print("\t\t..done.")
-    
+
     print("\tpulling from 'logs/shufflingData..'")
     shufflingFileList = os.listdir("logs/shufflingData")
     print("\t\t"+str(len(shufflingFileList))+" data points found.")
@@ -66,30 +72,38 @@ def ClusterInterface():
     print("\n#####################")
     print("# Printing results: #")
     print("#####################\n")
-    
+
     print("\n\t************************")
     print("\t* Walking Data labels: *")
     print("\t************************\n")
     print("\t\tData#\tFilename\tLabel")
     print("\t\t-----------------------------")
     for i in range(len(walkingFileList)):
-        print("\t\t" + str(i+1) + "\t" + walkingFileList[i] + "\t" + str(clusterer.results.labels_[i]))
-    
+        print("\t\t" + str(i+1) + "\t" +
+              walkingFileList[i] + "\t" +
+              str(clusterer.results.labels_[i]))
+
     print("\n\t*************************")
     print("\t* Standing Data labels: *")
     print("\t*************************\n")
-    print("\t\tData#\tFilename\tLabel") 
+    print("\t\tData#\tFilename\tLabel")
     print("\t\t-----------------------------")
     for i in range(len(standingFileList)):
-        print("\t\t" + str(i+1) + "\t" + standingFileList[i] + "\t" + str(clusterer.results.labels_[i + len(walkingFileList)]))
-    
+        print("\t\t" + str(i+1) + "\t" +
+              standingFileList[i] + "\t" +
+              str(clusterer.results.labels_[i + len(walkingFileList)]))
+
     print("\n\t**************************")
     print("\t* Shuffling Data labels: *")
     print("\t**************************\n")
     print("\t\tData#\tFilename\tLabel")
     print("\t\t-----------------------------")
     for i in range(len(shufflingFileList)):
-        print("\t\t" + str(i+1) + "\t" + shufflingFileList[i] + "\t" + str(clusterer.results.labels_[i + len(walkingFileList) + len(shufflingFileList) - 1]))
+        print("\t\t" + str(i+1) + "\t" +
+              shufflingFileList[i] + "\t" +
+              str(clusterer.results.labels_[i + len(walkingFileList) +
+                  len(shufflingFileList) - 1]))
+
 
 ClusterInterface()
 
