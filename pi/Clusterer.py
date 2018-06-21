d90f38a3bc7013931b0c02760b300e60f4e493db
Sammy Haq <sammy.haq1@gmail.com>
2018-06-21 14:26:40 -0400

    Changed structure of logs/ folder. Added command line arguments for Clusterer.py: default run does both tests. Adding KMeans does KMeans test. Adding SVC does SVC test. Also added SVC Clustering to Clusterer.py

--- pi/Clusterer.py
+++ pi/Clusterer.py
@@ -1,13 +1,15 @@
 from sklearn.cluster import KMeans
+from sklearn import svm
 import numpy as np
 import LoadFileHelper
 import os
+import sys
 
 
 class Clusterer:
 
     def __init__(self, filenames):
-        self.rawVariance = []
+        self.X = []
 
         for i in range(len(filenames)):
             loader = LoadFileHelper.LoadFileHelper(filenames[i])
@@ -19,31 +21,70 @@ class Clusterer:
             else:
                 loader.parseData()  # parsing data
 
-            self.rawVariance.append([loader.getDataVariance_X(),
-                                     loader.getDataVariance_Y(),
-                                     loader.getDataVariance_Z()])
+            self.X.append([loader.getDataVariance_X(),
+                           loader.getDataVariance_Y(),
+                           loader.getDataVariance_Z()])
 
-        self.results = KMeans(n_clusters=3,
-                              random_state=0).fit(np.array(self.rawVariance))
+    def doKMeansCluster(self):
+        return KMeans(n_clusters=3, random_state=0).fit(np.array(self.X))
+
+    def trainSVC(self):
+        self.Y = ["Standing", "Sitting", "Shuffling"]
+
+        self.clf = svm.SVC()
+        self.clf.fit(self.X, self.Y)
+
+    def predictSVC(self, feature):
+        return str(self.clf.predict(feature).item(0))
 
 
-def ClusterInterface():
+def execute():
+
+    print("\n####################################")
+    print("# Cluster Tester for Gaitmate Data #")
+    print("# Code by Sammy Haq                #")
+    print("# https://github.com/sammyhaq      #")
+    print("####################################\n")
+
+    runSVC = False
+    runKMeans = False
+
+    if (len(sys.argv) == 1):
+        runSVC = True
+        runKMeans = True
+    if ("SVC" in sys.argv):
+        runSVC = True
+    if ("KMeans" in sys.argv):
+        runKMeans = True
+
+    print("\n\t************************")
+    print("\t* Command Line Options *")
+    print("\t************************\n")
+    if (runSVC):
+        print("\t\tSVC Test: ENABLED")
+    else:
+        print("\t\tSVC Test: DISABLED")
+    if (runKMeans):
+        print("\t\tKMeans Test: ENABLED")
+    else:
+        print("\t\tKMeans Test: DISABLED")
+
     print("\n####################")
     print("# Gathering files: #")
     print("####################\n")
 
-    print("\tpulling from 'logs/walkingData..'")
-    walkingFileList = os.listdir("logs/walkingData")
+    print("\tpulling from 'logs/trainData/walkingData..'")
+    walkingFileList = os.listdir("logs/trainData/walkingData")
     print("\t\t"+str(len(walkingFileList))+" data points found.")
     print("\t\t..done.")
 
-    print("\tpulling from 'logs/standingData..'")
-    standingFileList = os.listdir("logs/standingData")
+    print("\tpulling from 'logs/trainData/standingData..'")
+    standingFileList = os.listdir("logs/trainData/standingData")
     print("\t\t"+str(len(standingFileList))+" data points found.")
     print("\t\t..done.")
 
-    print("\tpulling from 'logs/shufflingData..'")
-    shufflingFileList = os.listdir("logs/shufflingData")
+    print("\tpulling from 'logs/trainData/shufflingData..'")
+    shufflingFileList = os.listdir("logs/trainData/shufflingData")
     print("\t\t"+str(len(shufflingFileList))+" data points found.")
     print("\t\t..done.")
 
@@ -54,11 +95,11 @@ def ClusterInterface():
     print("# Appending file lists together.. #")
     print("###################################\n")
     for i in range(len(walkingFileList)):
-        fileList.append("logs/walkingData/" + walkingFileList[i])
+        fileList.append("logs/trainData/walkingData/" + walkingFileList[i])
     for i in range(len(standingFileList)):
-        fileList.append("logs/standingData/" + standingFileList[i])
+        fileList.append("logs/trainData/standingData/" + standingFileList[i])
     for i in range(len(shufflingFileList)):
-        fileList.append("logs/shufflingData/" + shufflingFileList[i])
+        fileList.append("logs/trainData/shufflingData/" + shufflingFileList[i])
     print("\t..done.\n")
 
     print("Total amount of data points: " + str(len(fileList)) + "\n")
@@ -69,42 +110,129 @@ def ClusterInterface():
     clusterer = Clusterer(fileList)
     print("\t..done.\n")
 
-    print("\n#####################")
-    print("# Printing results: #")
-    print("#####################\n")
+    if (runKMeans):
 
-    print("\n\t************************")
-    print("\t* Walking Data labels: *")
-    print("\t************************\n")
-    print("\t\tData#\tFilename\tLabel")
-    print("\t\t-----------------------------")
-    for i in range(len(walkingFileList)):
-        print("\t\t" + str(i+1) + "\t" +
-              walkingFileList[i] + "\t" +
-              str(clusterer.results.labels_[i]))
+        print("\n############################")
+        print("# Printing KMeans Results: #")
+        print("############################\n")
 
-    print("\n\t*************************")
-    print("\t* Standing Data labels: *")
-    print("\t*************************\n")
-    print("\t\tData#\tFilename\tLabel")
-    print("\t\t-----------------------------")
-    for i in range(len(standingFileList)):
-        print("\t\t" + str(i+1) + "\t" +
-              standingFileList[i] + "\t" +
-              str(clusterer.results.labels_[i + len(walkingFileList)]))
+        print("\n\t************************")
+        print("\t* Walking Data labels: *")
+        print("\t************************\n")
+        print("\t\tData#\tFilename\tLabel")
+        print("\t\t-----------------------------")
+        for i in range(len(walkingFileList)):
+            print("\t\t" + str(i+1) + "\t" +
+                  walkingFileList[i] + "\t" +
+                  str(clusterer.doKMeansCluster().labels_[i]))
 
-    print("\n\t**************************")
-    print("\t* Shuffling Data labels: *")
-    print("\t**************************\n")
-    print("\t\tData#\tFilename\tLabel")
-    print("\t\t-----------------------------")
-    for i in range(len(shufflingFileList)):
-        print("\t\t" + str(i+1) + "\t" +
-              shufflingFileList[i] + "\t" +
-              str(clusterer.results.labels_[i + len(walkingFileList) +
-                  len(shufflingFileList) - 1]))
+        print("\n\t*************************")
+        print("\t* Standing Data labels: *")
+        print("\t*************************\n")
+        print("\t\tData#\tFilename\tLabel")
+        print("\t\t-----------------------------")
+        for i in range(len(standingFileList)):
+            print("\t\t" + str(i+1) + "\t" +
+                  standingFileList[i] + "\t" +
+                  str(clusterer.doKMeansCluster().labels_[i +
+                      len(walkingFileList)]))
+
+        print("\n\t**************************")
+        print("\t* Shuffling Data labels: *")
+        print("\t**************************\n")
+        print("\t\tData#\tFilename\tLabel")
+        print("\t\t-----------------------------")
+        for i in range(len(shufflingFileList)):
+            print("\t\t" + str(i+1) + "\t" +
+                  shufflingFileList[i] + "\t" +
+                  str(clusterer.doKMeansCluster().labels_[i +
+                      len(walkingFileList) +
+                      len(shufflingFileList) - 1]))
+
+    if (runSVC): 
+        print("\n#########################")
+        print("# Printing SVC Results: #")
+        print("#########################\n")
+        
+        clusterer.trainSVC()
+
+        walkingTestFileList = os.listdir("logs/testData/walkingData")
+        standingTestFileList = os.listdir("logs/testData/standingData")
+        shufflingTestFileList = os.listdir("logs/testData/shufflingData")
+
+        walkingTestData = []
+        for file in range(len(walkingTestFileList)):
+            loader = LoadFileHelper.LoadFileHelper(
+                    "logs/testData/walkingData/"+walkingTestFileList[i])
+
+            # Checking to see if file was incorrectly saved, before parsing.
+            # Prevents blank files from being parsed and ruining clustering.
+            if (loader.isBlank()):
+                continue
+            else:
+                loader.parseData()  # parsing data
+
+            walkingTestData.append([loader.getDataVariance_X(),
+                                    loader.getDataVariance_Y(),
+                                    loader.getDataVariance_Z()])
+
+        standingTestData = []
+        for file in range(len(standingTestFileList)):
+            loader = LoadFileHelper.LoadFileHelper(
+                    "logs/testData/standingData/"+standingTestFileList[i])
+
+            if (loader.isBlank()):
+                continue
+            else:
+                loader.parseData()  # parsing data
+
+            standingTestData.append([loader.getDataVariance_X(),
+                                     loader.getDataVariance_Y(),
+                                     loader.getDataVariance_Z()])
+
+        shufflingTestData = []
+        for file in range(len(shufflingTestFileList)):
+            loader = LoadFileHelper.LoadFileHelper(
+                    "logs/testData/shufflingData/"+shufflingTestFileList[i])
+
+            if (loader.isBlank()):
+                continue
+            else:
+                loader.parseData()  # parsing data
+
+            standingTestData.append([loader.getDataVariance_X(),
+                                     loader.getDataVariance_Y(),
+                                     loader.getDataVariance_Z()])
+
+        print("\n\t*****************************")
+        print("\t* Walking Test Data Results *")
+        print("\t*****************************\n")
+        print("\t\tData#\tFilename\tLabel")
+        print("\t\t-----------------------------")
+        for i in range(len(walkingTestFileList)):
+            print("\t\t" + str(i+1) + "\t" +
+                  walkingTestFileList[i] + "\t" +
+                  str(clusterer.predictSVC(walkingTestData[i])))
+
+        print("\n\t******************************")
+        print("\t* Standing Test Data Results *")
+        print("\t******************************\n")
+        print("\t\tData#\tFilename\tLabel")
+        print("\t\t-----------------------------")
+        for i in range(len(standingTestFileList)):
+            print("\t\t" + str(i+1) + "\t" +
+                  standingTestFileList[i] + "\t" +
+                  str(clusterer.predictSVC(standingTestData[i])))
+
+        print("\n\t*******************************")
+        print("\t* Shuffling Test Data Results *")
+        print("\t*******************************\n")
+        print("\t\tData#\tFilename\tLabel")
+        print("\t\t-----------------------------")
+        for i in range(len(shufflingTestFileList)):
+            print("\t\t" + str(i+1) + "\t" +
+                  shufflingTestFileList[i] + "\t" +
+                  str(clusterer.predictSVC(standingTestData[i])))
 
 
-ClusterInterface()
-
-
+execute()
